35c05ef601450e4bacc2c9814f61cd7b7ae6914c
kimhung311 <72854974+kimhung311@users.noreply.github.com>
2024-08-21 10:23:44 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# demo-langchains-llm
+
+# create file venv and packages cmd
+  Step 1: python3 -m venv myvenv
+  Step 2: 
+    On Windows: myenv\Scripts\activate
+    On macOS/Linux: source myenv/bin/activate
+# install lang chains:
+  Step 3: pip install langchain
+  Step 4: pip install llama-cpp-python
+# start lang chains:  run file index_cpp.py
+  Step 5: python index_cpp.py
+
+
+
+   
+# demo-langchains
+# demo-langchains
+# demo-langchains
 # demo-langchains
---
+++ index_cpp.py
@@ -0,0 +1,16 @@
+from llama_cpp import Llama
+
+llm = Llama(model_path="./models/mistral-7b-openorca.Q4_K_S.gguf",
+            n_gpu_layers=1, n_ctx=4096)
+
+output = llm.create_completion("""<|im_start|>system
+You are a helpful chatbot.<|im_end|>
+                               
+<|im_start|>user
+                               
+Hello, tell me where can I learn Python?<|im_end|>
+                               
+<|im_start|>assistant""", max_tokens=500,  stop=["<|im_end|>"], stream=True)
+
+for token in output:
+    print(token["choices"][0]["text"], end='', flush=True)
